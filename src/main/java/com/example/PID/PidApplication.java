18bd2781e889e1b7408240af8cca8885d0d8a7f0
redou <redou@Redou>
2025-10-08 12:53:15 +0200

    Premier commit: installation du framework

---
+++ src/main/java/com/example/PID/PidApplication.java
@@ -0,0 +1,13 @@
+package com.example.PID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidApplication.class, args);
+	}
+
+}
