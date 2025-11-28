2abdfe7140b651c5b16ca0006352637a755cba7a
redou <redou@Redou>
2025-11-28 22:38:35 +0100

    After database configuration

---
+++ src/main/java/com/example/PID/reservationsspringboot/HomeController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.PID.reservationsspringboot;
+
+/**
+ *
+ * @author redou
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
