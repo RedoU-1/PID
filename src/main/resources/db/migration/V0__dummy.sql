338725d6f3096f256c6600297fa699fcd00bf75a
redou <redou@Redou>
2025-11-28 23:31:17 +0100

    V0__dummy.sql added

---
+++ src/main/resources/db/migration/V0__dummy.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  redou
+ * Created: 28 nov. 2025
+ */
+
