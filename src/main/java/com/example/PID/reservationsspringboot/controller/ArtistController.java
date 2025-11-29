0a7e67c1ea2971c18cb04034fe2e7910983c4b1b
redou <redou@Redou>
2025-11-30 00:15:32 +0100

    Controller Artist +templates +routes

--- src/main/java/com/example/PID/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/com/example/PID/reservationsspringboot/controller/ArtistController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller // Nous utiliserons @Controller plus tard avec Thymeleaf
 public class ArtistController {
@@ -29,5 +30,18 @@ public class ArtistController {
         model.addAttribute("title", "Liste des artistes"); // Ajoute un titre
 
         return "artist/index"; // Retourne le chemin vers le template Thymeleaf
+    }
+    
+    @GetMapping("/artists/{id}") // Capture l'ID dans l'URL
+    public String show(Model model, @PathVariable("id") long id) {
+        // 1. Récupère l'artiste via le Service
+        Artist artist = service.getArtist(id); 
+
+        // 2. Ajoute l'artiste au modèle pour la vue
+        model.addAttribute("artist", artist);
+        model.addAttribute("title", "Fiche d'un artiste");
+
+        // 3. Renvoie le nom du template (doit être créé)
+        return "artist/show";
     }
 }
