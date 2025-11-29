fc69894b4d70d5956c4e97cd23bbbf5733671b0a
redou <redou@Redou>
2025-11-30 00:44:44 +0100

    Form Delete Artist

--- src/main/java/com/example/PID/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/com/example/PID/reservationsspringboot/controller/ArtistController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -109,6 +110,18 @@ public class ArtistController {
 	    
 	    return "redirect:/artists/"+artist.getId();
 	}
+        
+        @DeleteMapping("/artists/{id}")
+	public String delete(@PathVariable long id, Model model) {
+	    Artist existing = service.getArtist(id);
+		
+	    if(existing!=null) {		
+	    	service.deleteArtist(id);
+	    }
+	    	    
+	    return "redirect:/artists";
+	}
+
 
         
 }
