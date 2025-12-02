5ba76a5742ec0f665b9fe7cb24acde2f3179f182
redou <redou@Redou>
2025-12-03 00:37:10 +0100

    Spring Security advanced configuration

--- src/main/java/com/example/PID/reservationsspringboot/config/SpringSecurityConfig.java
+++ src/main/java/com/example/PID/reservationsspringboot/config/SpringSecurityConfig.java
@@ -6,6 +6,10 @@ package com.example.PID.reservationsspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
@@ -18,35 +22,41 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
  * @author redou
  */
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(BCryptVersion.$2Y, 12);
+	return new BCryptPasswordEncoder(BCryptVersion.$2Y, 12);
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> { auth
-                    .requestMatchers("/").permitAll()
-                    .requestMatchers("/login", "/login**", "/css/**", "/js/**").permitAll()
-                    .requestMatchers("/admin").hasRole("ADMIN")
-                    .requestMatchers("/user").hasRole("MEMBER")
-                    .anyRequest().authenticated();
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.cors(Customizer.withDefaults())
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> {
+                	auth.requestMatchers("/admin").hasRole("ADMIN");
+                	auth.requestMatchers("/user").hasRole("MEMBER");
+                	auth.anyRequest().permitAll();
                 })
-                .formLogin((form) -> form
+                .formLogin(form -> form
                     .loginPage("/login")
                     .usernameParameter("login")
-                    .failureUrl("/login?loginError=true")
-                    .permitAll())
-                .logout((logout) -> logout
+                    .failureUrl("/login?loginError=true"))
+                .logout(logout -> logout
                     .logoutSuccessUrl("/login?logoutSuccess=true")
-                    .permitAll())
+                    .deleteCookies("JSESSIONID"))
                 .exceptionHandling(exception -> exception
                     .authenticationEntryPoint(
-                        new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
+			new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
                 .build();
     }
-
+    
 }
+
 
